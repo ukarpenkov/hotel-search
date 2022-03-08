f2f5a17f496556065e063641a25d30f491463a95
ukarpenkov <truloveu@gmail.com>
2022-03-08 14:07:02 +0300

    js to counter

--- src/index.js
+++ src/index.js
@@ -17,3 +17,29 @@ const user = {
 $('.block').html('jQuery is working');
 
 console.log(user)
+
+
+
+let counterButtons = document.querySelectorAll('.counter__btn')
+
+
+
+counterButtons.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let direction = this.dataset.direction
+        let value = this.parentElement.querySelectorAll('.dropdown-counter__value')
+
+        let currentValue = +value.innerHTML
+        let newValue
+
+        if (direction === 'plus') {
+            newValue = currentValue + 1
+            console.log('+++')
+        } else {
+            newValue = currentValue - 1
+            console.log('--')
+        }
+
+        value.innerHTML = newValue
+    })
+})
