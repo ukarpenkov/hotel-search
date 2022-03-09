01f5be049ea9015f31398a7f9b3d3d8843eb852b
ukarpenkov <truloveu@gmail.com>
2022-03-09 19:45:58 +0300

    js counter

--- src/index.js
+++ src/index.js
@@ -20,26 +20,32 @@ console.log(user)
 
 
 
-let counterButtons = document.querySelectorAll('.counter__btn')
+let counterButtons = document.querySelectorAll('.dropdown-counter__btn')
 
 
+function counter() {
+    let direction = this.dataset.direction
+    let value = this.parentElement.querySelector('.dropdown-counter__value')
+    let currentValue = +value.innerHTML
+    let newValue
+
+    if (direction === 'plus') {
+        newValue = currentValue + 1
+        console.log('+++')
+    }
+    else {
+        newValue = currentValue - 1
+        console.log('--')
+        if (currentValue === 0) {
+            newValue = 0
+
+        }
+    }
+    value.innerHTML = newValue
+
+}
 
 counterButtons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        let direction = this.dataset.direction
-        let value = this.parentElement.querySelectorAll('.dropdown-counter__value')
-
-        let currentValue = +value.innerHTML
-        let newValue
-
-        if (direction === 'plus') {
-            newValue = currentValue + 1
-            console.log('+++')
-        } else {
-            newValue = currentValue - 1
-            console.log('--')
-        }
-
-        value.innerHTML = newValue
-    })
+    btn.addEventListener('click', counter)
 })
+
