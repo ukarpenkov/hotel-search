5be2e11043288e15197627b944cda51811b4b7c8
ukarpenkov <truloveu@gmail.com>
2022-05-17 17:34:14 +0300

    cards-page html codding

--- src/index.js
+++ src/index.js
@@ -105,6 +105,18 @@ flatpickr(".calendar", {
 }
 );
 
+flatpickr(".visible-calendar", {
+    inline: true,
+    mode: "range",
+    altInput: true,
+    altFormat: "d.m.Y",
+    dateFormat: "Y-m-d\\Z",
+    "locale": Russian,
+    nextArrow: `<span class='material-icons subscribe-arrow' style='left:0px' >arrow_forward</span>`,
+    prevArrow: `<span class='material-icons subscribe-arrow' style='left:0px' >arrow_back</span>`
+}
+);
+
 
 
 var calendar = document.querySelectorAll('.flatpickr-calendar')
