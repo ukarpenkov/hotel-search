bc801b343724bb6e150a8d512987971fb848845d
ukarpenkov <truloveu@gmail.com>
2022-05-24 19:09:29 +0300

    room-details page diagramm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import $ from "jquery"
 import noUiSlider from 'nouislider'
 import flatpickr from 'flatpickr'
 import { Russian } from "flatpickr/dist/l10n/ru.js"
+import { Chart } from 'chart.js'
 
 
 const userStack = {
@@ -116,6 +117,42 @@ flatpickr(".visible-calendar", {
     prevArrow: `<span class='material-icons subscribe-arrow' style='left:0px' >arrow_back</span>`
 }
 );
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 
 
